e5c8206e9e091d801e34dd788d184cfe0e300dad
Jakub <kubasurdej@gmail.com>
2022-04-09 18:25:22 +0200

    Add swagger setup

--- src/config/base.config.ts
+++ src/config/base.config.ts
@@ -4,6 +4,7 @@ export const BASE_CONFIG_KEY = 'BASE_CONFIG_KEY';
 
 export const baseConfig = registerAs(BASE_CONFIG_KEY, () => ({
   port: +(process.env.PORT || 3000),
+  swaggerEnabled: process.env.NODE_ENV !== 'production',
 }));
 
 export type BaseConfig = ConfigType<typeof baseConfig>;
--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
 
+@ApiTags('health')
 @Controller('health')
 export class HealthController {
   constructor(private health: HealthCheckService) {}
